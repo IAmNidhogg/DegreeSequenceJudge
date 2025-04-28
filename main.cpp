a4fcb19306418c2e4f176265dd310ce2862da4b2
IAmNidhogg <810563833@qq.com>
2025-04-28 17:11:28 +0800

    correct recursion

--- main.cpp
+++ main.cpp
@@ -54,6 +54,7 @@ int main() {
     if (if_graph_seq) {
       std::cout << "是图序列" << std::endl;
     } else {
+      std::cout << "不是图序列" << std::endl;
       return 0;
     }
   }
@@ -108,7 +109,7 @@ bool judge_if_graph_seq(std::vector<int> &seq) {
     return false;
   }
 
-  return false;
+  return true;
 }
 
 void bubble_sort(std::vector<int> &seq) {
